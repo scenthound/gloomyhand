da1d03dcf180c418118d7b971e01c95f08a50b8b
Mike Lee <scenthound.sniff@gmail.com>
2017-06-24 18:03:04 -0400

    Using later react libraries

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', message="No Testa Me")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
